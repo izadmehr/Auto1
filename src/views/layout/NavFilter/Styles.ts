00df56e4e427d24870bcc49977fbe910bd73018d
izadmehr <meysam.s@gmail.com>
2019-07-15 11:53:30 +0300

    Move NavFilter to a directory

---
+++ src/views/layout/NavFilter/Styles.ts
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const NavFilterContainer = styled.nav`
+  width: 33.3%;
+`;
---
+++ src/views/layout/NavFilter/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import {NavFilterContainer} from './Styles';
+
+export function NavFilter(): JSX.Element {
+  return <NavFilterContainer />;
+}
