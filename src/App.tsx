fe23cc12b88340298939ee45d7b048404826349e
izadmehr <meysam.s@gmail.com>
2019-07-18 11:54:50 +0300

    Stick footer to the bottom

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,49 @@
 import { hot } from 'react-hot-loader/root';
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import styled from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
 
 import { Cars } from './views/cars';
 import { NoMatch } from './views/404';
 import { Header } from './views/layout/Header';
 import { Footer } from './views/layout/Footer';
+import { Show } from './views/Show';
 
 const Body = styled.main`
   font-family: 'Roboto', sans-serif;
-  margin: -8px;
+  min-height: 100%;
+  margin: -8px -8px -80px;
+`;
+const PushFooter = styled.div`
+  height: 80px;
+`;
+const GlobalStyle = createGlobalStyle`
+  html, body {
+  height: 100%;
+  margin: 0;
+}
+#root{
+ height: 100%;
+}
 `;
 
 function AppRouter(): JSX.Element {
   return (
-    <Router>
-      <Body>
-        <Header />
-        <Switch>
-          <Route path="/" exact component={Cars} />
-          <Route component={NoMatch} />
-        </Switch>
+    <>
+      <GlobalStyle />
+      <Router>
+        <Body>
+          <Header />
+          <Switch>
+            <Route path="/" exact component={Cars} />
+            <Route path="/car/:id" exact component={Show} />
+            <Route component={NoMatch} />
+          </Switch>
+          <PushFooter />
+        </Body>
         <Footer />
-      </Body>
-    </Router>
+      </Router>
+    </>
   );
 }
 
