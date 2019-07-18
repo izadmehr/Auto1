fb9a25996f07677a87cd43a9456f38d692d43021
izadmehr <meysam.s@gmail.com>
2019-07-18 23:14:18 +0300

    Show tests

--- src/views/Show/Show.test.tsx
+++ src/views/Show/Show.test.tsx
@@ -1,34 +1,146 @@
 // ETA: 2 hours
+import { render } from '@testing-library/react';
+import * as React from 'react';
+import Immutable from 'seamless-immutable';
+
+import { CarType } from '../../stores/selectedCar';
+
+import { ShowComponent } from './index';
+
+const car = Immutable<CarType>({
+  stockNumber: 9,
+  manufacturerName: 'Volkswagen',
+  modelName: 'Eos',
+  color: 'green',
+  mileage: {
+    number: 37175,
+    unit: 'km'
+  },
+  fuelType: 'Diesel',
+  pictureUrl: 'https://picsum.photos/id/111/200/200'
+});
+
 describe('<Show />', () => {
-  it('should redeirect to 404 if car is not found', () => {
-    expect(false).toBe(true);
-  });
+  // it('should redeirect to 404 if car is not found', () => {
+  //   expect(false).toBe(true);
+  // });
 
   it('should render car manufacturer name', () => {
-    expect(false).toBe(true);
+    const { getByText } = render(
+      <ShowComponent
+        match={{
+          params: { id: '1' },
+          isExact: true,
+          path: '/cars/1',
+          url: '/cars/1'
+        }}
+        getSelectedCar={(): void => {}}
+        car={car}
+      />
+    );
+
+    expect(getByText('Volkswagen', { exact: false })).toBeDefined();
   });
 
   it('should render car model name', () => {
-    expect(false).toBe(true);
+    const { getByText } = render(
+      <ShowComponent
+        match={{
+          params: { id: '1' },
+          isExact: true,
+          path: '/cars/1',
+          url: '/cars/1'
+        }}
+        getSelectedCar={(): void => {}}
+        car={car}
+      />
+    );
+
+    expect(getByText('Eos', { exact: false })).toBeDefined();
   });
 
   it('should render car stock number', () => {
-    expect(false).toBe(true);
+    const { getByText } = render(
+      <ShowComponent
+        match={{
+          params: { id: '1' },
+          isExact: true,
+          path: '/cars/1',
+          url: '/cars/1'
+        }}
+        getSelectedCar={(): void => {}}
+        car={car}
+      />
+    );
+
+    expect(getByText('#9', { exact: false })).toBeDefined();
   });
 
   it('should render car mileage', () => {
-    expect(false).toBe(true);
+    const { getByText } = render(
+      <ShowComponent
+        match={{
+          params: { id: '1' },
+          isExact: true,
+          path: '/cars/1',
+          url: '/cars/1'
+        }}
+        getSelectedCar={(): void => {}}
+        car={car}
+      />
+    );
+
+    expect(getByText('37175 km', { exact: false })).toBeDefined();
   });
 
-  it('should render car fule type', () => {
-    expect(false).toBe(true);
+  it('should render car fuel type', () => {
+    const { getByText } = render(
+      <ShowComponent
+        match={{
+          params: { id: '1' },
+          isExact: true,
+          path: '/cars/1',
+          url: '/cars/1'
+        }}
+        getSelectedCar={(): void => {}}
+        car={car}
+      />
+    );
+
+    expect(getByText('Diesel', { exact: false })).toBeDefined();
   });
 
   it('should render car color', () => {
-    expect(false).toBe(true);
+    const { getByText } = render(
+      <ShowComponent
+        match={{
+          params: { id: '1' },
+          isExact: true,
+          path: '/cars/1',
+          url: '/cars/1'
+        }}
+        getSelectedCar={(): void => {}}
+        car={car}
+      />
+    );
+
+    expect(getByText('green', { exact: false })).toBeDefined();
   });
 
   it('should render "Save" favorites <Button />', () => {
-    expect(false).toBe(true);
+    const { getByText } = render(
+      <ShowComponent
+        match={{
+          params: { id: '1' },
+          isExact: true,
+          path: '/cars/1',
+          url: '/cars/1'
+        }}
+        getSelectedCar={(): void => {}}
+        car={car}
+      />
+    );
+
+    expect(getByText('Save')).toBeDefined();
   });
 });
