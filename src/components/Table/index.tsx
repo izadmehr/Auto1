57d38cd7b96011a751e9899725b49976b29a5d46
izadmehr <meysam.s@gmail.com>
2019-07-20 11:45:44 +0300

    Move Table container to Cars

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,12 +1,8 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+// Component which renders list of items, e.g. car list
 
-import carsActions, {
-  CarsSelectors,
-  CarsType,
-  CarType
-} from '../../stores/cars';
-import { RootState } from '../../stores';
+import React from 'react';
+
+import { CarsType, CarType } from '../../stores/cars';
 import Row from './Row';
 import { CarsTableContainer } from './Styles';
 import { TableHeaderContainer } from './TableHeader';
@@ -15,50 +11,25 @@ import { Placeholder } from '../Placeholder';
 
 interface Props {
   cars: CarsType;
-  getCars: () => void;
   loadingStatus: boolean;
 }
 
 const placeholderIndexes = new Array(10).fill(0).map((key, index) => index);
 
-console.log('placeholderIndexes: ', placeholderIndexes);
-
-class Table extends Component<Props> {
-  componentDidMount(): void {
-    this.props.getCars();
-  }
-
-  render(): JSX.Element {
-    return (
-      <CarsTableContainer>
-        <TableHeaderContainer />
-        {this.props.loadingStatus
-          ? placeholderIndexes.map(
-              (key): JSX.Element => <Placeholder key={key} />
+export function Table(props: Props): JSX.Element {
+  return (
+    <CarsTableContainer>
+      <TableHeaderContainer />
+      {props.loadingStatus
+        ? placeholderIndexes.map(
+            (key): JSX.Element => <Placeholder key={key} />
+          )
+        : props.cars.map(
+            (car: CarType): JSX.Element => (
+              <Row key={car.stockNumber} car={car} />
             )
-          : this.props.cars.map(
-              (car: CarType): JSX.Element => (
-                <Row key={car.stockNumber} car={car} />
-              )
-            )}
-        <TablePaginationContainer />
-      </CarsTableContainer>
-    );
-  }
+          )}
+      <TablePaginationContainer />
+    </CarsTableContainer>
+  );
 }
-
-const mapStateToProps = (
-  state: RootState
-): { cars: CarsType; loadingStatus: boolean } => ({
-  cars: CarsSelectors.getData(state),
-  loadingStatus: CarsSelectors.getLoadingStatus(state)
-});
-
-const dispatchProps = {
-  getCars: carsActions.getCarsRequest
-};
-
-export const TableContainer = connect(
-  mapStateToProps,
-  dispatchProps
-)(Table);
--- src/views/cars/index.tsx
+++ src/views/cars/index.tsx
@@ -1,14 +1,48 @@
 import * as React from 'react';
+import { connect } from 'react-redux';
 
-import { TableContainer as Table } from '../../components/Table';
+import { Table } from '../../components/Table';
 import { CarsContainer } from './Styles';
 import { NavFilterContainer } from '../layout/NavFilter';
+import { RootState } from '../../stores';
+import carsActions, { CarsSelectors, CarsType } from '../../stores/cars';
 
-export const Cars = (): JSX.Element => {
-  return (
-    <CarsContainer>
-      <NavFilterContainer />
-      <Table />
-    </CarsContainer>
-  );
+interface Props {
+  cars: CarsType;
+  getCars: () => void;
+  loadingStatus: boolean;
+}
+
+export class CarsComponent extends React.Component<Props> {
+  componentDidMount(): void {
+    this.props.getCars();
+  }
+
+  render(): JSX.Element {
+    return (
+      <CarsContainer>
+        <NavFilterContainer />
+        <Table
+          loadingStatus={this.props.loadingStatus}
+          cars={this.props.cars}
+        />
+      </CarsContainer>
+    );
+  }
+}
+
+const mapStateToProps = (
+  state: RootState
+): { cars: CarsType; loadingStatus: boolean } => ({
+  cars: CarsSelectors.getData(state),
+  loadingStatus: CarsSelectors.getLoadingStatus(state)
+});
+
+const dispatchProps = {
+  getCars: carsActions.getCarsRequest
 };
+
+export const Cars = connect(
+  mapStateToProps,
+  dispatchProps
+)(CarsComponent);
