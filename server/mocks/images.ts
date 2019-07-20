69867923190207d96b6e29d49d27b767e96a30cb
izadmehr <meysam.s@gmail.com>
2019-07-20 15:26:57 +0300

    Increase size of car image mock

--- server/mocks/images.ts
+++ server/mocks/images.ts
@@ -1,9 +1,9 @@
 export const images = [
-  'https://picsum.photos/id/1071/200/200',
-  'https://picsum.photos/id/111/200/200',
-  'https://picsum.photos/id/133/200/200',
-  'https://picsum.photos/id/183/200/200',
-  'https://picsum.photos/id/45/200/200',
-  'https://picsum.photos/id/514/200/200',
-  'https://picsum.photos/id/655/200/200',
+  'https://picsum.photos/id/1071/800/800',
+  'https://picsum.photos/id/111/800/800',
+  'https://picsum.photos/id/133/800/800',
+  'https://picsum.photos/id/183/800/800',
+  'https://picsum.photos/id/45/800/800',
+  'https://picsum.photos/id/514/800/800',
+  'https://picsum.photos/id/655/800/800',
 ];
